c5c5d7afa32ac223b52ad175b5bbf32503198567
TharunVirtusa <engineerswish@gmail.com>
2023-08-13 15:32:01 +0000

    minor changes

--- decipherthepuzzle/src/App.js
+++ decipherthepuzzle/src/App.js
@@ -1,32 +1,35 @@
 
 import './App.css';
 import React,{useState} from'react';
+import {Login} from './components/Login';
+import {Register} from './components/Register';
 import {Button} from '@mui/material';
-import puzzleImages from './puzzleImages';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
+import { useDrag } from 'react-dnd'
+import Pieces from './components/Pieces';
 
 const rows = 4;
 const columns = 4;
- 
-const matrix = Array(rows).fill().map(() =>
-               Array(columns).fill(0));
 
+const matrix = Array(rows).fill().map(() =>Array(columns).fill(0));
 
-function createPieces(){
-  return (React.createElement('div',{className:'pieces'}));
-}
-
-function App() {
-    const [changeStyle,shufflePieces]=useState({
-        position:"absolute",
-        left:Math.floor((Math.random()*300))+ "px",
-        top:Math.floor((Math.random()*300))+ "px",
-     });
-     function IsClicked(){
-       shufflePieces();
-       return true;
-     }
-  return (
+  function App() {
+   var [startButtonClicked,toggle]=useState(false);
+   const [currForm,setCurrForm]=useState('register');
+   function startButtonClickHandler(){
+        
+      toggle(startButtonClicked=true);
+   }
+   function resetButtonClickHandler(){
+    toggle(startButtonClicked=false);
+   }
+   const toggleForm=(formName)=>{
+      setCurrForm(formName);
+   }
+    return (
     <div className="App">
+     { currForm==="login"?<Login onSwitch={toggleForm}/>:<Register onSwitch={toggleForm}/>}
       <header className="App-header">
          Solve the puzzle
       </header>
@@ -36,21 +39,34 @@ function App() {
          </div>
          <div className="puzzleContainer">
          {matrix.map((item,index)=>{
-            return(
-              <div>
-               {item.map((subItem,subIndex)=>
-                 <div 
-                   className='pieces' 
-                   style={IsClicked?{backgroundPositionX:-100*subIndex,backgroundPositionY:-100*index}:{changeStyle}}>
-                 </div>
-               )}
-              </div>
-            );
-         })}
+               return(
+                <div>
+                   {item.map((subItem,subIndex)=>
+                      <div 
+                         className="pieces"
+                         style={startButtonClicked?{
+                         position:"absolute",
+                         cursor: 'move',
+                         left:Math.floor((Math.random()*290))+ "px",
+                         top:Math.floor((Math.random()*290))+ "px",
+                         }:{backgroundPositionX:-100*subIndex,backgroundPositionY:-100*index}}>
+                      </div>
+                   )}
+                </div>
+                );
+            })
+          }
          </div>
       </div>
-      <Button variant="contained" color="info" onClick={IsClicked}>Decipher</Button>
-      <Button id="reset" variant="contained" color="success">Reset</Button>
+      <Button 
+         style={startButtonClicked?{display:"none"}:{display:"block"}} 
+         variant="contained" 
+         color="info" 
+         onClick={startButtonClickHandler}>Start</Button>
+      <Button 
+         style={startButtonClicked?{display:"block"}:{display:"none"}} 
+         variant="contained" color="success" 
+         onClick={resetButtonClickHandler}>Reset Puzzle</Button>
     </div>
   );
 }
--- decipherthepuzzle/src/components/Pieces.js
+++ decipherthepuzzle/src/components/Pieces.js
@@ -1,11 +1,21 @@
 import React from 'react';
 
 function Pieces(props){
+   
+}
+function setBorder(){
+  grid.map((i)=>{
     return(
-       <div>
-        <img alt="piece" src={props.image}/>
-       </div>
+      <div>
+       {i.map(()=>
+         <div 
+           className="pieces"
+           style="background-image:none">
+         </div>
+       )}
+      </div>
     );
+  });
 
 }
 export default Pieces;
